6bd7cc197c41f4961e77417c8460e032a46d1945
Tonaion02 <antonioambrosiopub@gmail.com>
2026-02-26 10:17:52 +0100

    ordered files and cleaned the project

--- bitset.h
+++ bitset.h
@@ -18,19 +18,30 @@
 // T: refer to examples that are contained in the README.md to understand how.
 // The basic implementation allocate memory on the heap using the malloc function.
 // In this implementation is also possible to decide what is the function that is used to allocate
-// memory on the heap. We can redefine the macros: _allocate_mem, _free_mem.  
+// memory on the heap. You can do that redefining the macros: _allocate_mem, _free_mem.  
 //
 //
 // TO-DO:
-// - Add a macro(or at least an example) that brings you possibility to handle allocation of memory outside of this function
-// - Probably there is a great problem with the fact that i can't use the numbers that i am using
-// - Add some operations:
-//      - Give the possibility to enlarge the bitset, so to extend the bitset out of the current size.
-//      - Create a macro to copy an entire bitset from an existing bitset.
-//      - Create a macro to allocate the bitset on the stack.
+// - Add the missing operations: print_word_bitset
+// - Add the possibility to extend the current bitset, like an std::vector
+// - Add an operation to copy bitset
+// - Add give the user more freedom for allocation of memory: possibility to allocate on the stack or to make a total custom allocation
+// - Give to the user the possibility to allocate in a single piece of memory size, capacity and packed_data, or 
+// to allocate (size, capacity) and then packed_data
+// - Re-write the doc
+// Things to check:
+// - Check if prints only the bits for size and not the entire capacity
+// - Check if all the ASSERT works
 //==================================================================================================================================
 #ifndef _BITSET
 #define _BITSET
+
+
+
+// T: TODO It's necessary to check if it is possible to remove this include from here.
+#include <stdint.h> 
+#include <stdlib.h>
+#include <stdio.h>
 #include "custom_assert.h"
 
 
@@ -45,22 +56,22 @@
 
 typedef struct 
 {
-    unsigned int size;              // T: number of bits that are currently stored in bitset
-    unsigned int capacity;          // T: number of words of type WORD_TYPE that are occupied in memory to store bits
+    uint32_t size;              // T: number of bits that are currently stored in bitset
+    uint32_t capacity;          // T: number of words of type WORD_TYPE that are occupied in memory to store bits
     // T: This is a specific manner to specify a pointer in C.
-    // T: Is is useful to allocate the entier bitset like a single piece of memory even if the buffer packed_data has a
-    // variable length.
+    // T: It is useful to allocate the entire struct CBitSet like a single piece of memory, even if packed_data has
+    // a variable length.
     // We can directly allocate memory for the entire bitset in this way(capacity is the number of words to allocate
     // to store the bit that we need to store):
-    // Bitset *pointer = (Bitset*)malloc(sizeof(Bitset) + sizeof(WORD_TYPE) * size));
+    // Bitset *pointer = (CBitSet*)malloc(sizeof(CBitSet) + sizeof(WORD_TYPE) * size));
     // In this way packed_data start directly from the address next to capacity.
-    // T: How we already sayed, the advantage to use this trick is that now the allocation of the Bitset is entirely contigous.
-    WORD_TYPE packed_data[0];       
-} Bitset;
+    // T: How we already sayed, the advantage to use this trick is that all the CBitSet is allocated in contigous memory.
+    WORD_TYPE packed_data[0];
+} CBitSet;
 
 
 
-// T: size of the word used in *bit*
+// T: Size of the word in bit
 #define WORD_SIZE (sizeof(WORD_TYPE) * 8)
 
 
@@ -71,116 +82,134 @@ typedef struct
 // T: WARNING private macro
 // T: This macro is used to access the correct position of a WORD in the bitset
 #define _internal_index(i) i % WORD_SIZE
-// T: Macros used to handle memory (START)
+
+// T: TODO This macro is useful to get the number of words that are really used by the bitset
+#define _size_in_word(size) (size / WORD_SIZE + (int)((size % WORD_SIZE) >= 1))
+
+
+
+// T: Macros for memory (START)
 // T: These macros are used to specify which functions are used to allocate and free memory on the heap.
-// It is useful to easily adapt this implementation of bitset to all platform where we don't have an
-// implemenentation of malloc and free.
-// T: WARNING private macro
 #define _allocate_mem malloc
-// T: WARNING private macro
 #define _free_mem free
-// T: Macros used to handle memory (END)
+// T: Macros for memory (END)
 
-// T: Default allocation of a bitset is made on the heap.
-// T: TODO try to fix the fact that the allocation of biset is made only on the heap. Give the possibility to chose where we want to allocate the bitset.
-#define create_bitset(bitset_p, _size)                                              \
-    {                                                                               \
-        int t = _size / WORD_SIZE + (int)((_size % WORD_SIZE) >= 1);                \
-        bitset_p = (Bitset*) _allocate_mem(sizeof(Bitset) + t * sizeof(WORD_TYPE)); \
-        bitset_p->size = _size;                                                     \
-        bitset_p->capacity = t;                                                     \
-    } 
-                                   
-#define free_bitset(bitset_p)   \
-    _free_mem(bitset_p);
 
-#define set_true(bitset_p, i)                                                                   \
-    ASSERT(i < bitset_p->size)                                                                  \
-    bitset_p->packed_data[_word_index((i))] |= 1 << (WORD_SIZE - 1 - _internal_index((i)));
 
-#define set_false(bitset_p, i)                                                                  \
-    ASSERT(i < bitset_p->size)                                                                  \
-    bitset_p->packed_data[_word_index((i))] &= ~(1 << (WORD_SIZE - 1 - _internal_index((i))));
+CBitSet* create_bitset(uint32_t _size)
+{
+    uint32_t capacity = _size_in_word(_size);
+    CBitSet* p = (CBitSet*)_allocate_mem(sizeof(CBitSet) + sizeof(WORD_TYPE) * capacity);
+    ASSERT(p != NULL)
+    p->capacity = capacity;
+    p->size = _size;
 
-#define set_bit(bitset_p, i, bit)           \
-    ASSERT(i < bitset_p->size)                                                      \
-    bitset_p->packed_data[_word_index((i))] = (bitset_p->packed_data[_word_index((i))] & ~((!bit) << (WORD_SIZE - 1 - _internal_index((i))))  ) | ((bit) << (WORD_SIZE - 1 - _internal_index((i))));
+    return p;
+}
 
-// T: This macro doesn't end with ";" because the result of the operation computed in this macro is probably used in other expresions.
-// For example in assignment expressions. 
-#define get_bit(bitset, i)                                                                      \
-    ASSERT(i < bitset_p->size)                                                                  \
-    bitset->packed_data[_word_index((i))] & (1 << (WORD_SIZE - 1 - _internal_index((i))))
+void free_bitset(CBitSet* bitset)
+{
+    _free_mem(bitset);
+}
 
-#define all_true(bitset_p)                              \
-    {                                                   \
-        for(int i = 0; i < bitset_p->capacity; i++)     \
-        {                                               \
-            bitset_p->packed_data[i] |= ~0;             \
-        }                                               \
+void set_true(CBitSet* bitset, uint32_t i)
+{
+    ASSERT(i < bitset->size)
+    bitset->packed_data[_word_index(i)] |= (1 << (_internal_index(i)));
+}
+
+void set_false(CBitSet* bitset, uint32_t i)
+{
+    ASSERT(i < bitset->size)
+    bitset->packed_data[_word_index(i)] &= ~(1 << (_internal_index(i)));
+}
+
+// T: TODO Think if it is the best type to return
+uint8_t get_bit(CBitSet* bitset, uint32_t i)
+{
+    ASSERT(i < bitset->size)
+    return bitset->packed_data[_word_index(i)] & (1 << _internal_index(i));
+}
+
+// T: TODO Think if it is the best type to represent a bit
+void set_bit(CBitSet* bitset, uint32_t i, uint8_t bit)
+{
+    ASSERT(i < bitset->size)
+    // T: It's necessary this assert, set_bit work only with bit = 0 or bit = 1.
+    ASSERT(bit == 1 || bit == 0)
+    bitset->packed_data[_word_index(i)] |= (bit << (_internal_index(i)));
+    bitset->packed_data[_word_index(i)] &= ~(!bit << (_internal_index(i)));
+}
+
+void all_true(CBitSet* bitset)
+{
+    // T: OPT Probably I can optimize iterating on capacity
+    for(uint32_t i = 0; i < bitset->capacity; i++)
+    {
+        bitset->packed_data[i] |= (WORD_TYPE)~0;
     }
-#define all_false(bitset_p)                             \
-    {                                                   \
-        for(int i = 0; i < bitset_p->capacity; i++)     \
-        {                                               \
-            bitset_p->packed_data[i] &= 0;              \
-        }                                               \
+}
+
+void all_false(CBitSet* bitset)
+{
+    // T: OPT Probably I can optimize iterating on capacity
+    for(uint32_t i = 0; i < bitset->capacity; i++)
+    {
+        bitset->packed_data[i] &= 0;
     }
+}
 
-// T: NOTE this operation assume that bitset_1 and bitset_2 have the same size
-#define and(bitset_1, bitset_2)                                                                  \
-    {                                                                                            \
-        ASSERT(bitset_1->size == bitset_2->size)                                                 \
-        for(int i = 0; i < bitset_1->capacity; i++)                                              \
-        {                                                                                        \
-            bitset_1->packed_data[i] &= bitset_2->packed_data[i];                                \
-        }                                                                                        \
+// T: TODO Think about the fact that probably it's useful to implement
+// "and" and "or" operations even for bitset of different size.
+// T: and method perform the an and bit a bit operation between the first bitset
+// and the second bitset.
+// T: The result of the and operation is stored in the first bitset, the second
+// bitset isn't modified at all.
+void and(CBitSet* bitset1, CBitSet* bitset2)
+{
+    ASSERT(bitset1->size == bitset2->size)
+    for(uint32_t i = 0; i < _size_in_word(bitset1->size); i++)
+    {
+        bitset1->packed_data[i] &= bitset2->packed_data[i];
     }
+}
 
-// T: NOTE this operation assume that biset_1 and bitset_2 have the same size
-#define or(bitset_1, bitset_2)                                                                          \
-    {   \
-        ASSERT(bitset_1->size == bitset_2->size) \
-        for(int i = 0; i < bitset_1->capacity; i++) \
-        {   \
-            bitset_1->packed_data[i] |= bitset_2->packed_data[i];   \
-        }   \
+void or(CBitSet* bitset1, CBitSet* bitset2)
+{
+    ASSERT(bitset1->size == bitset2->size)
+    for(uint32_t i = 0; i < _size_in_word(bitset1->size); i++)
+    {
+        bitset1->packed_data[i] |= bitset2->packed_data[i];
+    }    
+}
+
+void not(CBitSet* bitset)
+{
+    // T: OPT Probably it's not necessary to set negate even the bits that are out of size
+    for(uint32_t i = 0; i < bitset->capacity; i++)
+    {
+        bitset->packed_data[i] = ~bitset->packed_data[i];
     }
+}
 
-#define not(bitset_p)                                                                           \
-    {\
-        for(int i = 0; i < bitset_p->capacity; i++)\
-        {\
-            bitset_p->packed_data[i] = ~bitset_p->packed_data[i];\
-        }\
-    } 
-
-#define print_word_bitset(word) \
-    { \
-        int i; \
-        for(i = (sizeof(WORD_TYPE) * 8) -1; i >= 0; i--) \
-        { \
-        int b = word & (1 << i);   \
-        printf("%d", (int)(b != 0)); \
-        } \
-    } 
-
-#define print_bitset(bitset_p) \
-    { \
-        for(int i = 0; i < bitset_p->capacity; i++) \
-        { \
-            for(int j = WORD_SIZE -1; j >= 0; j--) \
-            {   \
-                if(i * WORD_SIZE + j < bitset_p->size) \
-                { \
-                    int b = bitset_p->packed_data[i] & (1 << _internal_index(j)); \
-                    printf("%d", (int)(b != 0));    \
-                } \
-            } \
-            printf("\n"); \
-        } \
+void print_bitset(CBitSet* bitset)
+{
+    // T: NOTE Indexes i,j are handled deciding to go from 1..capacity or 1..WORD_SIZE,
+    // even if the valid values goes from 0..(capacity-1) and 0..(WORD_SIZE-1) because
+    // in this way I can define them like uint32_t and match with indexes the types
+    // of capacity and size field of the bitset.
+    for(uint32_t i = bitset->capacity; i >= 1; i--)
+    {
+        for(uint32_t j = WORD_SIZE; j >=1; j--)
+        {
+            if((i-1) * WORD_SIZE + (j-1) < bitset->size)
+            {
+                int b = bitset->packed_data[i-1] & (1 << (j-1));
+                printf("%d", (int)(b != 0));
+            }
+        }
+        printf("\n");
     }
-//==================================================================================================================================
-// BITSET
-//==================================================================================================================================
+}
+
 #endif
--- test_bitset.c
+++ test_bitset.c
@@ -8,85 +8,60 @@
 
 
 int main()
-{    
-    // Bitset *bitset;
-
-    // int n = 45;
-    // create_bitset(bitset, n);
- 
-    // // T: print current capacity of Bitset
-    // // T: NOTE please note that the capacity of Bitset reppresent the number of "WORDS"
-    // // that are used to store the bits. The effective number of stored bits are size.
-    // printf("capacity of bitset: %d\n", bitset->capacity);
-
-    // printf("size of bitset: %d\n", bitset->size);
-    
-    // // T: put all the bits of bitset to true
-    // all_true(bitset);
-    // printf("word 2 of bitset: \n");
-    // print_word_bitset(bitset->packed_data[2]);
-    // printf("\n");
-    // // printBitset(bitset);
-    
-    // // T: put all bits of bitset to false
-    // all_false(bitset);
-    // printf("word 2 of bitset: \n");
-    // print_word_bitset(bitset->packed_data[2]);
-    // printf("\n");
-    // // printBitset(bitset);
-
-
-    
-    // T: TODO implement this idea for a stack allocated bitset.
-    // T: NOTE you can use this trick even to allocate multiple bitset like a single piece of memory
+{
     const unsigned int n = 10;
-    const unsigned int n_char_words = 2 * 4 + n * sizeof(WORD_TYPE);
-    char buffer[n_char_words];
-    Bitset *p = (Bitset*)buffer; 
-    p->capacity = n;
-    p->size = n * WORD_SIZE;
+    CBitSet *p = create_bitset(n * WORD_SIZE);
 
     printf("capacity of bitset: %d\n", p->capacity);
-
     printf("size of bitset: %d\n", p->size);
     
     all_true(p);
-    print_word_bitset(p->packed_data[2]);
+    print_bitset(p);
     printf("\n");
 
     set_false(p, 2 * WORD_SIZE + 2);
-    print_word_bitset(p->packed_data[2]);
+    print_bitset(p);
     printf("\n");
+
+    set_bit(p, 2 * WORD_SIZE + 2, 1);
+    printf("After set bit\n");
+    print_bitset(p);
+
+    set_bit(p, 2 * WORD_SIZE + 2, 0);
+    printf("After the second set bit\n");
+    print_bitset(p);
 
     printf("print bitset\n");
     print_bitset(p);
 
-    const unsigned int n2 = 10;
-    const unsigned int n_char_words_2 = 2 * 4 + n2 * sizeof(WORD_TYPE);
-    char buffer2[n_char_words_2];
-    Bitset *p_2 = (Bitset*)buffer2;
-    p->capacity = n2;
-    p->size = n2 * WORD_SIZE;
+    // const unsigned int n2 = 10;
+    // // const unsigned int n_char_words_2 = 2 * 4 + n2 * sizeof(WORD_TYPE);
+    // // char buffer2[n_char_words_2];
+    // // Bitset *p_2 = (Bitset*)buffer2;
+    // // p->capacity = n2;
+    // // p->size = n2 * WORD_SIZE;
 
-    all_true(p_2);
-    printf("and between p, p_2\n");
-    and(p, p_2);
-    print_bitset(p);
+    // CBitSet *p_2 = create_bitset(n2 * WORD_SIZE);
 
-    printf("or between p, p_2\n");
-    or(p, p_2);
-    print_bitset(p);
+    // all_true(p_2);
+    // printf("and between p, p_2\n");
+    // and(p, p_2);
+    // print_bitset(p);
+
+    // printf("or between p, p_2\n");
+    // or(p, p_2);
+    // print_bitset(p);
     
-    printf("not of p\n");
-    not(p);
-    print_bitset(p);
+    // printf("not of p\n");
+    // not(p);
+    // print_bitset(p);
 
-    all_true(p);
-    printf("\n");
-    print_bitset(p);
-    set_bit(p, 3, 0);
-    printf("\n");
-    print_bitset(p);
+    // all_true(p);
+    // printf("\n");
+    // print_bitset(p);
+    // set_bit(p, 3, 0);
+    // printf("\n");
+    // print_bitset(p);
 
     return 0;
 }
